0276cc8bb8061ddc858bc1d04acb6f2dc976d27c
SAHIL RAJAPKAR <70615373+THSSIR18@users.noreply.github.com>
2021-02-01 01:08:48 -0500

    Add files via upload

---
+++ AverageTwo.java
@@ -0,0 +1,16 @@
+/*
+	Myles Lesser
+	String Demo
+	April 22,2015
+*/
+public class AverageTwo
+{
+	public static void main (String[] args)
+	{
+		int firstNumber = 9;
+		int secondNumber =3;
+		double average = (firstNumber + secondNumber) / 2;
+		System.out.println("The average is " + average);
+	}
+}
+	
